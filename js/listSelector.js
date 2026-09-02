fe1f8453861c20d7934d39cc0b9422fad72eac59
rosesrdead <enenen1024@gmail.com>
2026-09-02 23:08:32 +0200

    Update listSelector.js

--- js/listSelector.js
+++ js/listSelector.js
@@ -9,23 +9,25 @@ function renderListCheckboxes() {
     db.lists.forEach(list => {
 
         const label = document.createElement("label");
-        label.className = "listCard";
 
         const checkbox = document.createElement("input");
+
         checkbox.type = "checkbox";
+
         checkbox.value = list.id;
 
-        const icon = document.createElement("div");
-        icon.className = "listCardIcon";
-        icon.textContent = list.icon || "📝";
-
-        const name = document.createElement("div");
-        name.className = "listCardName";
-        name.textContent = list.name;
 
         label.appendChild(checkbox);
-        label.appendChild(icon);
-        label.appendChild(name);
+
+
+        const icon = list.icon || "📝";
+
+        label.appendChild(
+            document.createTextNode(
+                icon + " " + list.name
+            )
+        );
+
 
         listContainer.appendChild(label);
 
